325b24dbd73c47e2bdf3e60a325709d02e48e2d0
Kiffin Gish <kiffin.gish@planet.nl>
2017-04-17 22:06:55 +0200

    plugins directory

--- config/initializers/geolocation.rb
+++ config/initializers/geolocation.rb
@@ -1,8 +1,8 @@
-Geolocation.configure do |config|
-  config.enabled = true
-  config.file_name = 'data_dump.csv'
-  config.upload_dir = 'uploads'
-  config.allow_blank = false
-  config.delete_all = true
-  config.max_lines = 0
-end
+# Geolocation.configure do |config|
+#   config.enabled = true
+#   config.file_name = 'data_dump.csv'
+#   config.upload_dir = 'uploads'
+#   config.allow_blank = false
+#   config.delete_all = true
+#   config.max_lines = 0
+# end
